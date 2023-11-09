a713cb2dda0ce810df53f94f2e09361301c74820
MarlonWiss <marlon.wisskirchen@zewotherm.de>
2023-11-09 09:41:02 +0100

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:mvc_task_school/view.dart';
 
+// GitHub link: https://github.com/MarlonWiss/mvc_task_school
+
 void main() {
-  runApp(FluentApp(
+  runApp(
+    FluentApp(
       title: 'MVC Task School',
       theme: FluentThemeData(
         brightness: Brightness.light,
